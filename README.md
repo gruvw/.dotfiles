304e354f9ef3a2e1f3cee3cd495c6214046b7327
gruvw <gruvw.dev@gmail.com>
2022-01-05 15:12:28 +0100

    Updated install script

---
+++ README.md
@@ -0,0 +1,3 @@
+# DotFiles
+
+Repository installation: `curl -s https://dev.gruvw.com/init.sh | sh` (or `wget -qO - https://dev.gruvw.com/init.sh | sh`)
--- http/init.sh
+++ http/init.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-# Git install
-git --version > /dev/null 2>&1
-if ! [ $? -eq 0 ]; then
-    echo "Git is not already installed. Installing..."
+# Git installed check
+git --version >/dev/null 2>&1
+GIT_INSTALLED=$?
+
+# Stow installed check
+stow --version >/dev/null 2>&1
+STOW_INSTALLED=$?
+
+# Stop execution on error
+set -e
+
+# Install git
+if ! [ $GIT_INSTALLED -eq 0 ]; then
+    echo "+ Git is not already installed. Installing..."
     sudo apt install git -y
 fi
 
+# Install stow
+if ! [ $STOW_INSTALLED -eq 0 ]; then
+    echo "+ Stow is not already installed. Installing..."
+    sudo apt install stow -y
+fi
+
 # Clone dotfiles repo to home directory
-echo "Cloning dotfiles repo to home directory..."
+echo "+ Cloning gruvw/dotfiles repo to home directory..."
 git clone https://github.com/gruvw/dotfiles.git ~/dotfiles
 
-echo "Done!"
+echo "+ Done!"
