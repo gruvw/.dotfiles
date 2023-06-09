ccccc608deb8a12b59427bf60c44228177f5a023
gruvw <gruvw.dev@gmail.com>
2023-06-09 11:00:48 +0200

    Fixes OS with LT

--- others/keyboard/main.py
+++ others/keyboard/main.py
@@ -33,7 +33,7 @@ split = Split(
     data_pin2=data_pin2
 )
 
-keyboard.modules += [oneshot, layers, split, combos]
+keyboard.modules += [oneshot, combos, split, layers]
 
 # Keys
 
@@ -62,7 +62,7 @@ OS_LSFT_LALT_LGUI = KC.OS(KC.LSFT(KC.LALT(OS_LGUI)), tap_time=None)
 OS_LCTL_LSFT_LALT_LGUI = KC.OS(KC.LCTL(KC.LSFT(KC.LALT(OS_LGUI))), tap_time=None)
 
 NUM_LAY = KC.LT(1, KC.BSPC, tap_time=200, prefer_hold=True, tap_interrupted=True)
-FUN_LAY = KC.LT(2, KC.NO, tap_time=0)
+FUN_LAY = KC.LT(2, KC.DOT, tap_time=200, prefer_hold=True)
 
 # Combos
 
@@ -87,7 +87,7 @@ combos.combos = [
 keyboard.keymap = [
     # 0: DVORAK
     [
-        KC.NO,   OS_LALT, OS_LGUI, KC.NO,   KC.P,    KC.Y,                          KC.F,    KC.G,    KC.C,    KC.R,   KC.L,  KC.NO,
+        KC.NO,   KC.RALT, OS_LGUI, OS_LALT, KC.P,    KC.Y,                          KC.F,    KC.G,    KC.C,    KC.R,   KC.L,  KC.NO,
         KC.NO,   KC.A,    KC.O,    KC.E,    KC.U,    KC.I,                          KC.D,    KC.H,    KC.T,    KC.N,   KC.S,  KC.NO,
         KC.NO,   KC.ESC,  KC.Q,    KC.J,    KC.K,    KC.X,                          KC.B,    KC.M,    KC.W,    KC.V,   KC.Z,  KC.NO,
                                             KC.TAB,  KC.SPC,  KC.ENTER,    OS_LSFT, NUM_LAY, OS_LCTL,
@@ -96,16 +96,16 @@ keyboard.keymap = [
     # 1: Numbers / Symbols 1
     [
         KC.NO,   KC.GRV,  KC.LT,   KC.LCBR, KC.LPRN, KC.LBRC,                       KC.COMM, KC.EXLM, KC.QUES, KC.DQUO, KC.QUOT, KC.NO,
-        KC.NO,   KC.N0,   KC.N1,   KC.N2,   KC.N3,   KC.N4,                         KC.PLUS, KC.MINS, KC.ASTR, KC.SLSH, KC.DOT,  KC.NO,
-        KC.NO,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,                         KC.BSLS, KC.UNDS, KC.AMPR, KC.CIRC, KC.COLN, KC.NO,
+        KC.NO,   KC.N0,   KC.N1,   KC.N2,   KC.N3,   KC.N4,                         KC.PLUS, KC.MINS, KC.ASTR, KC.SLSH, KC.COLN, KC.NO,
+        KC.NO,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,                         KC.BSLS, KC.UNDS, KC.CIRC, KC.AMPR, KC.PIPE, KC.NO,
                                             KC.EQL,  FUN_LAY, KC.SCLN,     KC.NO,   KC.TRNS, KC.NO,
     ],
 
     # 2: Function / Symbols 2
     [
-        KC.NO,   DEG,     KC.GT,   KC.RCBR, KC.RPRN, KC.RBRC,                       KC.F10,  KC.TILD, KC.NO,   KC.NO,   KC.NO,     KC.NO,
-        KC.NO,   KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,                         KC.F11,  KC.AT,   KC.DLR,  KC.PERC, KC.HASH,   KC.NO,
-        KC.NO,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,                         KC.F12,  KC.NO,   KC.PIPE, KC.NO,   KC.NO,     KC.NO,
+        KC.NO,   DEG,     KC.GT,   KC.RCBR, KC.RPRN, KC.RBRC,                       KC.F10,  KC.TILD, KC.NO,   KC.NO,   KC.NO,   KC.NO,
+        KC.NO,   KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,                         KC.F11,  KC.AT,   KC.DLR,  KC.PERC, KC.HASH, KC.NO,
+        KC.NO,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,                         KC.F12,  KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,
                                             KC.NO,   KC.TRNS,   KC.NO,     KC.NO,   KC.TRNS, KC.NO
     ]
 ]
