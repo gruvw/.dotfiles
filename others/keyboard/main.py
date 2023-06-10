1b7fb70174c0e4cd8713fd6c8d1a1adf272b407c
gruvw <gruvw.dev@gmail.com>
2023-06-10 09:36:48 +0200

    Caps word

--- others/keyboard/main.py
+++ others/keyboard/main.py
@@ -3,10 +3,11 @@ import digitalio
 
 from kb import KMKKeyboard, isRight
 from kmk.consts import UnicodeMode
-from storage import getmount
 
 from kmk.keys import KC
+from kmk.modules.tapdance import TapDance
 from kmk.modules.layers import Layers
+from kmk.modules.capsword import CapsWord
 from kmk.modules.oneshot import OneShot
 from kmk.modules.combos import Chord, Combos
 from kmk.handlers.sequences import unicode_string_sequence
@@ -15,6 +16,7 @@ from kmk.modules.split import Split, SplitSide, SplitType
 # PARAMS
 
 OS_TIMEOUT = 1000
+OS_TAP_TIME = 1000
 TAP_TIME = 200
 
 # SETUP
@@ -24,6 +26,7 @@ keyboard = KMKKeyboard()
 layers = Layers()
 oneshot = OneShot()
 combos = Combos()
+caps_word = CapsWord()
 
 split_side = SplitSide.RIGHT if isRight else SplitSide.LEFT
 
@@ -38,32 +41,30 @@ split = Split(
     data_pin2=data_pin2
 )
 
-keyboard.modules += [oneshot, combos, split, layers]
-
 # Keys
 
 keyboard.unicode_mode = UnicodeMode.LINUX
 
 DEG = unicode_string_sequence("°")
 
-OS_LCTL = KC.OS(KC.LCTL, tap_time=TAP_TIME)
-OS_LSFT = KC.OS(KC.LSFT, tap_time=TAP_TIME)
-OS_LGUI = KC.OS(KC.LGUI, tap_time=TAP_TIME)
-OS_LALT = KC.OS(KC.LALT, tap_time=TAP_TIME)
+OS_LCTL = KC.OS(KC.LCTL, tap_time=OS_TAP_TIME)
+OS_LSFT = KC.OS(KC.LSFT, tap_time=OS_TAP_TIME)
+OS_LGUI = KC.OS(KC.LGUI, tap_time=OS_TAP_TIME)
+OS_LALT = KC.OS(KC.LALT, tap_time=OS_TAP_TIME)
 
-OS_LCTL_LSFT = KC.OS(KC.LCTL(OS_LSFT), tap_time=TAP_TIME)
-OS_LCTL_LALT = KC.OS(KC.LCTL(OS_LALT), tap_time=TAP_TIME)
-OS_LCTL_LGUI = KC.OS(KC.LCTL(OS_LGUI), tap_time=TAP_TIME)
-OS_LSFT_LALT = KC.OS(KC.LSFT(OS_LALT), tap_time=TAP_TIME)
-OS_LSFT_LGUI = KC.OS(KC.LSFT(OS_LGUI), tap_time=TAP_TIME)
-OS_LALT_LGUI = KC.OS(KC.LALT(OS_LGUI), tap_time=TAP_TIME)
+OS_LCTL_LSFT = KC.OS(KC.LCTL(OS_LSFT), tap_time=OS_TAP_TIME)
+OS_LCTL_LALT = KC.OS(KC.LCTL(OS_LALT), tap_time=OS_TAP_TIME)
+OS_LCTL_LGUI = KC.OS(KC.LCTL(OS_LGUI), tap_time=OS_TAP_TIME)
+OS_LSFT_LALT = KC.OS(KC.LSFT(OS_LALT), tap_time=OS_TAP_TIME)
+OS_LSFT_LGUI = KC.OS(KC.LSFT(OS_LGUI), tap_time=OS_TAP_TIME)
+OS_LALT_LGUI = KC.OS(KC.LALT(OS_LGUI), tap_time=OS_TAP_TIME)
 
-OS_LCTL_LSFT_LGUI = KC.OS(KC.LCTL(KC.LSFT(OS_LGUI)), tap_time=TAP_TIME)
-OS_LCTL_LSFT_LALT = KC.OS(KC.LCTL(KC.LSFT(OS_LALT)), tap_time=TAP_TIME)
-OS_LCTL_LALT_LGUI = KC.OS(KC.LCTL(KC.LALT(OS_LGUI)), tap_time=TAP_TIME)
-OS_LSFT_LALT_LGUI = KC.OS(KC.LSFT(KC.LALT(OS_LGUI)), tap_time=TAP_TIME)
+OS_LCTL_LSFT_LGUI = KC.OS(KC.LCTL(KC.LSFT(OS_LGUI)), tap_time=OS_TAP_TIME)
+OS_LCTL_LSFT_LALT = KC.OS(KC.LCTL(KC.LSFT(OS_LALT)), tap_time=OS_TAP_TIME)
+OS_LCTL_LALT_LGUI = KC.OS(KC.LCTL(KC.LALT(OS_LGUI)), tap_time=OS_TAP_TIME)
+OS_LSFT_LALT_LGUI = KC.OS(KC.LSFT(KC.LALT(OS_LGUI)), tap_time=OS_TAP_TIME)
 
-OS_LCTL_LSFT_LALT_LGUI = KC.OS(KC.LCTL(KC.LSFT(KC.LALT(OS_LGUI))), tap_time=TAP_TIME)
+OS_LCTL_LSFT_LALT_LGUI = KC.OS(KC.LCTL(KC.LSFT(KC.LALT(OS_LGUI))), tap_time=OS_TAP_TIME)
 
 NUM_LAY = KC.LT(1, KC.BSPC, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
 FUN_LAY = KC.LT(2, KC.DOT, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
@@ -108,9 +109,9 @@ keyboard.keymap = [
 
     # 2: Function / Symbols 2
     [
-        KC.NO,   DEG,     KC.NO,   KC.NO,   KC.NO,   KC.NO,                       KC.F10,  KC.TILD, KC.NO,   KC.NO,   KC.NO,      KC.NO,
-        KC.NO,   KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,                         KC.F11,  KC.AT,   KC.DLR,  KC.PERC, KC.HASH,  KC.NO,
-        KC.NO,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,                         KC.F12,  KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+        KC.NO,   DEG,     KC.NO,   KC.F10,  KC.F11,  KC.F12,                        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+        KC.NO,   KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,                         KC.TILD, KC.AT,   KC.DLR,  KC.PERC, KC.HASH,  KC.NO,
+        KC.NO,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
                                             KC.NO,   KC.TRNS,   KC.NO,     KC.NO,   KC.TRNS, KC.NO
     ],
 
@@ -119,11 +120,13 @@ keyboard.keymap = [
         KC.NO,   KC.NO,   KC.LABK, KC.LCBR, KC.LPRN, KC.LBRC,                       KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.RIGHT, KC.NO,
         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.LEFT, KC.NO,   KC.NO,   KC.NO,    KC.NO,
         KC.NO,   KC.NO,   KC.NO,   KC.DOWN, KC.UP,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
-                                            KC.NO,   KC.TRNS,   KC.NO,     KC.NO,   KC.NO, KC.NO
+                                            KC.NO,   KC.TRNS,   KC.NO,     KC.CW,   KC.NO, KC.NO
     ]
 ]
 
 # MAIN
+
+keyboard.modules += [oneshot, combos, split, layers, caps_word]
 
 if __name__ == '__main__':
     keyboard.go()
