bd246ab9012b4d48de45351f904f9f1ea0d0071b
gruvw <gruvw.dev@gmail.com>
2023-06-11 19:52:24 +0200

    Updated kepmap

--- others/keyboard/main.py
+++ others/keyboard/main.py
@@ -1,23 +1,24 @@
 import board
-import digitalio
 
 from kb import KMKKeyboard, isRight
 from kmk.consts import UnicodeMode
 
 from kmk.keys import KC
-from kmk.modules.tapdance import TapDance
 from kmk.modules.layers import Layers
 from kmk.modules.capsword import CapsWord
 from kmk.modules.oneshot import OneShot
 from kmk.modules.combos import Chord, Combos
-from kmk.handlers.sequences import unicode_string_sequence
+from kmk.modules.dynamic_sequences import DynamicSequences
+from kmk.handlers.sequences import unicode_string_sequence, send_string
 from kmk.modules.split import Split, SplitSide, SplitType
 
 # PARAMS
 
 OS_TIMEOUT = 1000
-OS_TAP_TIME = 1000
-TAP_TIME = 200
+DS_TIMEOUT = 60 * 1000
+
+OS_TAP_TIME = 2000
+TAP_TIME = 500
 
 # SETUP
 
@@ -29,7 +30,6 @@ combos = Combos()
 caps_word = CapsWord()
 
 split_side = SplitSide.RIGHT if isRight else SplitSide.LEFT
-
 data_pin = board.GP1 if split_side == SplitSide.LEFT else board.GP0
 data_pin2 = board.GP0 if split_side == SplitSide.LEFT else board.GP1
 
@@ -41,11 +41,14 @@ split = Split(
     data_pin2=data_pin2
 )
 
-# Keys
+dynamic_sequences = DynamicSequences(timeout=DS_TIMEOUT)
 
 keyboard.unicode_mode = UnicodeMode.LINUX
 
+# Keys
+
 DEG = unicode_string_sequence("°")
+HTP = send_string("https://")
 
 OS_LCTL = KC.OS(KC.LCTL, tap_time=OS_TAP_TIME)
 OS_LSFT = KC.OS(KC.LSFT, tap_time=OS_TAP_TIME)
@@ -66,9 +69,17 @@ OS_LSFT_LALT_LGUI = KC.OS(KC.LSFT(KC.LALT(OS_LGUI)), tap_time=OS_TAP_TIME)
 
 OS_LCTL_LSFT_LALT_LGUI = KC.OS(KC.LCTL(KC.LSFT(KC.LALT(OS_LGUI))), tap_time=OS_TAP_TIME)
 
-NUM_LAY = KC.LT(1, KC.BSPC, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
+MGC_LAY = KC.LT(5, KC.TAB, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
+SP1_LAY = KC.LT(3, KC.ENTER, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
+SP2_LAY = KC.LT(4, KC.DEL, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
+NUM_LAY = KC.LT(1, KC.BSPC, tap_time=200, prefer_hold=True, tap_interrupted=True)
 FUN_LAY = KC.LT(2, KC.DOT, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
-SPC_LAY = KC.LT(3, KC.SPC, tap_time=TAP_TIME, prefer_hold=True, tap_interrupted=True)
+
+DS_REC = KC.RECORD_SEQUENCE()
+DS_STP = KC.STOP_SEQUENCE()
+DS_PLA = KC.PLAY_SEQUENCE()
+DS_REP = KC.SET_SEQUENCE_REPETITIONS()
+DS_INT = KC.SET_SEQUENCE_INTERVAL()
 
 # Combos
 
@@ -90,18 +101,22 @@ combos.combos = [
 
 # LAYERS
 
+# TODO Media keys
+# TODO boot
+# TODO mouse keys
+
 keyboard.keymap = [
     # 0: DVORAK
     [
         KC.NO,   KC.RALT, OS_LGUI, OS_LALT, KC.P,    KC.Y,                          KC.F,    KC.G,    KC.C,    KC.R,    KC.L,      KC.NO,
         KC.NO,   KC.A,    KC.O,    KC.E,    KC.U,    KC.I,                          KC.D,    KC.H,    KC.T,    KC.N,    KC.S,      KC.NO,
         KC.NO,   KC.ESC,  KC.Q,    KC.J,    KC.K,    KC.X,                          KC.B,    KC.M,    KC.W,    KC.V,    KC.Z,      KC.NO,
-                                            KC.TAB,  SPC_LAY,  KC.ENTER,   OS_LSFT, NUM_LAY, OS_LCTL,
+                                            MGC_LAY, KC.SPC,   SP1_LAY,    OS_LSFT, NUM_LAY, OS_LCTL,
     ],
 
     # 1: Numbers / Symbols 1
     [
-        KC.NO,   KC.GRV,  KC.RABK, KC.RCBR, KC.RPRN, KC.RBRC,                       KC.COMM, KC.EXLM, KC.QUES, KC.DQUO, KC.QUOT,  KC.NO,
+        KC.NO,   KC.GRV,  KC.LABK, KC.LCBR, KC.LPRN, KC.LBRC,                       KC.COMM, KC.EXLM, KC.QUES, KC.DQUO, KC.QUOT,  KC.NO,
         KC.NO,   KC.N0,   KC.N1,   KC.N2,   KC.N3,   KC.N4,                         KC.PLUS, KC.MINS, KC.ASTR, KC.SLSH, KC.COLN,  KC.NO,
         KC.NO,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,                         KC.BSLS, KC.UNDS, KC.CIRC, KC.AMPR, KC.PIPE,  KC.NO,
                                             KC.EQL,  FUN_LAY, KC.SCLN,     KC.NO,   KC.TRNS, KC.NO,
@@ -112,21 +127,45 @@ keyboard.keymap = [
         KC.NO,   DEG,     KC.NO,   KC.F10,  KC.F11,  KC.F12,                        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
         KC.NO,   KC.NO,   KC.F1,   KC.F2,   KC.F3,   KC.F4,                         KC.TILD, KC.AT,   KC.DLR,  KC.PERC, KC.HASH,  KC.NO,
         KC.NO,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
-                                            KC.NO,   KC.TRNS,   KC.NO,     KC.NO,   KC.TRNS, KC.NO
+                                            KC.NO,   KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.NO
     ],
 
-    # 3: Special keys
+    # 3: Special keys 1
     [
-        KC.NO,   KC.NO,   KC.LABK, KC.LCBR, KC.LPRN, KC.LBRC,                       KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.RIGHT, KC.NO,
+        KC.NO,   KC.NO,   KC.RABK, KC.RCBR, KC.RPRN, KC.RBRC,                       KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.RIGHT, KC.NO,
         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.LEFT, KC.NO,   KC.NO,   KC.NO,    KC.NO,
         KC.NO,   KC.NO,   KC.NO,   KC.DOWN, KC.UP,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
-                                            KC.NO,   KC.TRNS,   KC.NO,     KC.CW,   KC.NO, KC.NO
-    ]
+                                            KC.NO,   KC.TRNS, KC.NO,       KC.CW,   SP2_LAY, KC.NO
+    ],
+
+    # 4: Special keys 2
+    [
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.END,   KC.NO,
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.HOME, KC.NO,   KC.NO,   KC.NO,    KC.NO,
+        KC.NO,   KC.NO,   KC.NO,   KC.PGDN, KC.PGUP, KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+                                            KC.NO,   KC.TRNS, KC.NO,       KC.NO,   KC.TRNS, KC.NO
+    ],
+
+    # 5: Magic
+    [
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         DS_REP,  DS_REC,  DS_STP,  DS_PLA,  DS_INT,   KC.NO,
+        KC.NO,   HTP,     KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+                                            KC.TRNS, KC.NO,   KC.NO,       KC.NO,   KC.NO,   KC.NO
+    ],
+
+    # X: Dummy
+    # [
+    #     KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+    #     KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+    #     KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,                         KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,    KC.NO,
+    #                                         KC.NO,   KC.NO,   KC.NO,       KC.NO,   KC.NO,   KC.NO
+    # ],
 ]
 
 # MAIN
 
-keyboard.modules += [oneshot, combos, split, layers, caps_word]
+keyboard.modules += [dynamic_sequences, oneshot, combos, layers, split, caps_word]
 
 if __name__ == '__main__':
     keyboard.go()
