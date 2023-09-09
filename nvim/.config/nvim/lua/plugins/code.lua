461f45252d8145b60ccef007d19c8c420d0d9e82
gruvw <gruvw.dev@gmail.com>
2023-09-09 13:36:38 +0200

    Text objects

--- nvim/.config/nvim/lua/plugins/code.lua
+++ nvim/.config/nvim/lua/plugins/code.lua
@@ -5,11 +5,32 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    dependencies = {
+      -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = "all",
         highlight = {
           enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = {query = "@class.inner", desc = "Select inner part of a class region"},
+              ["as"] = {query = "@scope", query_group = "locals", desc = "Select language scope"},
+              ["ib"] = "@block.inner",
+              ["ab"] = "@block.outer",
+              ["il"] = "@loop.inner",
+              ["al"] = "@loop.outer",
+            },
+          },
         },
       })
     end,
