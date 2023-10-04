b04c1e679f6d43f55f26670a37851a757430a590
gruvw <gruvw.dev@gmail.com>
2023-10-04 13:32:03 +0200

    Update indent-blankline to v3

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -135,25 +135,29 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-      require("indent_blankline").setup({
-        char = "│",
-        show_end_of_line = true,
+      require("ibl").setup({
+        -- show_end_of_line = true,
         -- show_trailing_blankline_indent = false,
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
+        indent = {
+          char = "▏",
+          highlight = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+            "IndentBlanklineIndent3",
+            "IndentBlanklineIndent4",
+            "IndentBlanklineIndent5",
+            "IndentBlanklineIndent6",
+          },
         },
-        filetype_exclude = {
-          "lspinfo",
-          "packer",
-          "checkhealth",
-          "help",
-          "man",
-          "",
+        exclude = {
+          filetypes = {
+            "lspinfo",
+            "packer",
+            "checkhealth",
+            "help",
+            "man",
+            "",
+          },
         },
       })
     end
